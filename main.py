f44cd4497bef93588c94f7e69192344e42dd06e1
Flint Louis <flintlouis@Flints-MacBook-Pro.local>
2021-09-19 23:51:27 +0200

    added negamax

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-from Game import next_player, Game
-from minmax import minmax, random_move, randint, math, get_states
+from Game import Game
+from minmax import minmax, negamax, random_move, randint, math, get_states
 import numpy as np
 import os
 
 def main():
 	game = Game()
-	player = randint(1, 2)
+	player = randint(-1, 1)
 	os.system("clear")
 	game.print_state()
 	while True:
 		if player == 1:
 			best_score = -math.inf
-			for row in range(3):
-				for col in range(3):
-					if game[row, col] == 0:
-						game[row, col] = player
-						score = minmax(game, 10, -math.inf, math.inf,False, next_player(player))
-						game[row, col] = 0
-						if score > best_score:
-							best_score = score
-							best_move = (row, col)
+			for place in game.get_empty_places():
+				game[place] = player
+				score = negamax(game, 10, -math.inf, math.inf, -player)
+				# score = minmax(game, 10, -math.inf, math.inf, False, -player)
+				game[place] = 0
+				if score > best_score:
+					best_score = score
+					best_move = place
 			game[best_move] = player
 		else:
 			row = int(input("row: "))
@@ -27,7 +26,7 @@ def main():
 			if not game[row, col] == 0:
 				continue
 			game[row, col] = player
-		player = next_player(player)
+		player = -player
 		os.system("clear")
 		game.print_state()
 		if 0 not in game.state or game.is_finished():
--- minmax.py
+++ minmax.py
@@ -1,6 +1,6 @@
 import math
+from os import stat
 from random import randint
-from Game import next_player
 
 states = 0
 
@@ -14,37 +14,49 @@ def random_move(game):
 			return row, col
 
 def minmax(game, depth, alpha, beta, maximizing_player, player):
-	finished = game.is_finished()
-	if not finished == None:
-		return game.get_score(finished)
+	global states
+	score = game.is_finished()
+	if not score == None:
+		states += 1
+		return score
 	if depth == 0:
 		return 0
-	global states
 	if maximizing_player:
 		max_eval = -math.inf
-		for row in range(3):
-			for col in range(3):
-				if game[row, col] == 0:
-					game[row, col] = player
-					states += 1
-					eval = minmax(game, depth - 1, alpha, beta, False, next_player(player))
-					game[row, col] = 0
-					max_eval = max(max_eval, eval)
-					alpha = max(alpha, eval)
-					if beta <= alpha:
-						break
+		for place in game.get_empty_places():
+			game[place] = player
+			eval = minmax(game, depth - 1, alpha, beta, False, -player)
+			game[place] = 0
+			max_eval = max(max_eval, eval)
+			alpha = max(alpha, eval)
+			if beta <= alpha:
+				break
 		return max_eval
 	else:
 		min_eval = +math.inf
-		for row in range(3):
-			for col in range(3):
-				if game[row, col] == 0:
-					game[row, col] = player
-					states += 1
-					eval = minmax(game, depth - 1, alpha, beta, True, next_player(player))
-					game[row, col] = 0
-					min_eval = min(min_eval, eval)
-					beta = min(beta, eval)
-					if beta <= alpha:
-						break
+		for place in game.get_empty_places():
+			game[place] = player
+			states += 1
+			eval = minmax(game, depth - 1, alpha, beta, True, -player)
+			game[place] = 0
+			min_eval = min(min_eval, eval)
+			beta = min(beta, eval)
+			if beta <= alpha:
+				break
 		return min_eval
+
+def negamax(game, depth, alpha, beta, player):
+	score = game.is_finished()
+	if not score == None:
+		return player * score
+	if depth == 0:
+		return 0
+	value = -math.inf
+	for place in game.get_empty_places():
+		game[place] = player
+		value = max(value, negamax(game, depth - 1, -beta, -alpha, -player))
+		game[place] = 0
+		alpha = max(alpha, value)
+		if alpha >= beta:
+			break
+	return -value
